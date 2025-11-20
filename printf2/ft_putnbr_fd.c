a751bcf23b96df3ee2f5b67a2e20da03c2d08e57
Logosh <101455421+Bearhart-corp@users.noreply.github.com>
2025-11-20 22:26:28 +0100

    Update ft_putnbr_fd.c

--- printf2/ft_putnbr_fd.c
+++ printf2/ft_putnbr_fd.c
@@ -22,25 +22,35 @@ static size_t	h(long long n, t_flags f, t_nbr *s)
 
 	i = -1;
 	count = 0;
+	if (!(s->isleft))
+		while (s->pad_width-- > 0)
+			tmp[count++] = s->sym_pad;
 	while (n > 0)
 	{
-		tmp[count++] = "0123456789abcdef"[n % s->base];
+		tmp[count++] = "0123456789abcdef"[n & s->base];
 		n = n / s->base;
 	}
-	if (f.conv == PTR || f.flags & HASH)
-	{
-		tmp[count++] = 'x';
+	while (s->prefix[++i])
+		tmp[count++] = s->prefix[i];
+	i = - 1;
+	while (s->pad_prec-- > s->n_digit)
 		tmp[count++] = '0';
-	}
+	if (s->sign_len)
+		tmp[count++] = s->sign;
+	count = 0;
+	if (s->isleft)
+		while (s->pad_width-- > 0)
+			tmp[count++] = s->sym_pad;
 	while (++i < count)
 		s->buf[i] = tmp[(count - 1) - i];
 	return (count);
 }
-
+//pad - sign - prec - prefix - digit - pad
 size_t	ft_putnbr(long n, t_flags f)
 {
 	t_nbr	s;
 	
 	init(n, f, 1, &s);
+	h(n, f, s);
 	return (h())
 }
