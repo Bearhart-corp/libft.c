e3f5e70ec019598620e4c7a35a6540814cabc244
Thomas Belard <tbelard@k1r3p4.42mulhouse.fr>
2025-11-21 18:52:45 +0100

    add printf3

---
+++ printf3/ft_putfloat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putfloat.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbelard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 17:53:22 by tbelard           #+#    #+#             */
+/*   Updated: 2025/11/07 17:53:24 by tbelard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	power(char pow)
+{
+	int	acc;
+	int	power;
+
+	power = (int)pow;
+	acc = 1;
+	while (power--)
+		acc = acc * 10;
+	return (acc);
+}
+
+size_t	ft_putfloat(double n, t_flags f)
+{
+	long	n_int;
+	size_t	count;
+
+	count = 0;
+	if (n < 0)
+	{
+		n = -n;
+		count += write(1, "-", 1);
+	}
+	n += (0.5 / power(f.prec));
+	n_int = (long)n;
+	count += ft_putnbr(n_int, f);
+	count += write(1, ".", 1);
+	n = (n - n_int) * power(f.prec);
+	return (count += ft_putnbr(n, f));
+}
