d5defa7540322b4c58cef5832914813a148d768f
Logosh <101455421+Bearhart-corp@users.noreply.github.com>
2025-11-25 23:57:12 +0100

    Update get_next_line_utils.c

--- GNL/get_next_line_utils.c
+++ GNL/get_next_line_utils.c
@@ -12,22 +12,39 @@
 
 #include "get_next_line.h"
 
-void	init(t_var *s, char *buf, int fd)
+void	init(t_var *s, char *buf_fd)
 {
-	s->tmp = NULL;
 	s->cap = BUFFER_SIZE + 1;
 	s->line = malloc(s->cap);
-	s->len = 0;
-	if (*buf)
+	if (!s->line)
 	{
-		ft_memmove(s->line, buf, ft_strchr(buf, 0, BUFFER_SIZE));
-		s->len = ft_strlen(s->line);
+		s->cap = 0;
+		s->len = 0;
+		s->byte = -1;
+		return ;
 	}
-	s->byte = read(fd, buf, BUFFER_SIZE);
-	if (s->byte >= 0)
-   		buf[s->byte] = 0;
-	s->addr = NULL;
+	s->len = 0;
+	s->i = 0;
+	s->byte = (ssize_t)ft_strlen(buf_fd);
 }
+
+int	ensure_cap(t_var *s)
+{
+	char	*new_line;
+
+	if (s->cap >= s->len + BUFFER_SIZE + 1)
+		return (1);
+	while (s->cap < s->len + BUFFER_SIZE + 1)
+		s->cap <<= 1;
+	new_line = s->line;
+	s->line = malloc(s->cap);
+	if (!s->line)
+		return (0);
+	ft_memmove(s->line, new_line, s->len);
+	free(new_line);
+	return (1);
+}
+
 
 size_t	ft_strlen(const char *s)
 {
@@ -56,19 +73,71 @@ char	*ft_strchr(const char *s, int c, size_t n)
 	return (NULL);
 }
 
-void	*ft_memmove(void *dest, const void *src, char *add_end)
+void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
-	unsigned char		*ad;
 
-	d = (unsigned char *)dest;
+	if (!n || dst == src)
+		return (dst);
+	d = (unsigned char *)dst;
 	s = (const unsigned char *)src;
-	ad = (unsigned char *)add_end;
-	if (dest == src)
-		return (dest);
-	while (s <= ad)
+	if (d < s)
+		while (n--)
 			*d++ = *s++;
-		*d = 0;
-	return(dest);
+	else
+		while (n--)
+			d[n] = s[n];
+	return (dst);
 }
+
+void	*fast_memmove(void *dst, void *src, size_t n)
+{
+    if (!n || dst == src)
+		return (dst);
+    if (dst < src)
+    {
+        while (n-- && ((uintptr_t)dst & 0b111))
+			*(uint8_t *)dst++ = *(uint8_t *)src++;
+        while (n >= 8)
+        {
+            n -= 8;
+            *(uint64_t *)dst++ = *(uint64_t *)src++;
+        }
+        while (n--)
+            *(uint8_t *)dst++ = *(uint8_t *)src++;
+    }
+    else
+    {
+        while (n-- && ((uintptr_t)dst & 0b111))
+			*(uint8_t *)(dst + n) = *(uint8_t *)(src + n);
+        while (n >= 8)
+        {
+            n -= 8;
+            *(uint64_t *)(dst + n) = *(uint64_t *)(src + n);
+        }
+        while (n--)
+            *(uint8_t *)(dst + n) = *(uint8_t *)(src + n);
+    }
+    return (dst);
+}
+ /*#include <immintrin.h>
+
+void *ft_memcpy_avx2(void *dst, const void *src, size_t n)
+{
+    unsigned char *d = dst;
+    const unsigned char *s = src;
+
+    while (n >= 32)
+    {
+        __m256i chunk = _mm256_loadu_si256((__m256i*)s);
+        _mm256_storeu_si256((__m256i*)d, chunk);
+        s += 32;
+        d += 32;
+        n -= 32;
+    }
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+*/
