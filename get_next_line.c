8b67cd00aa633729006365b32b58efbbb8273154
Logosh <101455421+Bearhart-corp@users.noreply.github.com>
2025-11-25 16:48:38 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_s.line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbelard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 12:58:25 by tbelard           #+#    #+#             */
+/*   Updated: 2025/11/25 12:58:27 by tbelard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ret_line(t_var s, char *buf)
+{
+	ft_memmove(s.left, buf, s.addr);
+	ft_memmove(buf, s.addr + 1, s.tmp);
+	ft_memmove(s.line + s.len, s.left, s.left + ft_strlen(s.left));
+	return (s.line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buf[OPEN_MAX][BUFFER_SIZE + 1];
+	t_var		s;
+
+	init(&s, buf[fd], fd);
+	while (s.byte > 0)
+	{
+		if (s.cap < s.len + BUFFER_SIZE)
+		{
+			while (s.cap < s.len + BUFFER_SIZE)
+				s.cap = s.cap << 1;
+			s.tmp = s.line;
+			s.line = malloc(s.cap);
+			ft_memmove(s.line, s.tmp, s.tmp + s.len);
+			free(s.tmp);
+		}
+		s.tmp = buf[fd] + BUFFER_SIZE - 1;
+		s.addr = ft_strchr(buf[fd], 10, BUFFER_SIZE);
+		if (s.addr)
+			return (ret_line(s, buf[fd]));
+		ft_memmove(s.line + s.len, buf[fd], s.tmp);
+		s.byte = read(fd, buf[fd], BUFFER_SIZE);
+		s.len += s.byte;
+	}
+	return (s.line);
+}
+
+int main()
+{
+	int fd = open("test.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	
+}
+//1234\n
+//12345\n
+//1234512345\n
---
+++ get_next_line_utils.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbelard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/18 15:51:02 by tbelard           #+#    #+#             */
+/*   Updated: 2025/11/18 15:51:03 by tbelard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	init(t_var *s, char *buf, int fd)
+{
+	s->tmp = NULL;
+	s->cap = BUFFER_SIZE + 1;
+	s->line = malloc(s->cap);
+	s->len = 0;
+	if (*buf)
+	{
+		ft_memmove(s->line, buf, ft_strchr(buf, 0, BUFFER_SIZE));
+		s->len = ft_strlen(s->line);
+	}
+	s->byte = read(fd, buf, BUFFER_SIZE);
+	s->addr = NULL;
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	if (!s)
+		return (0);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c, size_t n)
+{
+	int	i;
+
+	i = -1;
+	while (s[++i] && n--)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+	}
+	if (s[i] == (char)c && !s[i])
+		return ((char *)s + i);
+	return (NULL);
+}
+
+void	*ft_memmove(void *dest, const void *src, char *add_end)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	unsigned char		*ad;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	ad = (unsigned char *)add_end;
+	if (dest == src)
+		return (dest);
+	while (s <= ad)
+			*d++ = *s++;
+		*d = 0;
+	return(dest);
+}
